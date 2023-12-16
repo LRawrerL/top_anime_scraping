9a47d0f6d406361fac1ab0f5e4b56c96ce853b19
LRawrerL <yixuanliang05@gmail.com>
2023-12-17 00:38:05 +0800

    Implement getting all titles from a single page

--- index.js
+++ index.js
@@ -22,8 +22,24 @@ const getTitles =  async () => {
         return {rank, title, score, information};
     });
 
-    console.log(firstTitle);
-    await browser.close();
+    let isTherePage = true;
+
+    while(isTherePage){
+        const allTitlesInaPage = await page.evaluate(() => {
+            const data = document.querySelectorAll(".ranking-list");
+            
+            return Array.from(data).map((show) => {
+                const rank = show.querySelector(".top-anime-rank-text").innerHTML;
+                const title = show.querySelector(".anime_ranking_h3").innerText;
+                const score = show.querySelector(".score-label").innerText;
+                const information = show.querySelector(".information").innerText
+                return {rank, title, score, information};
+            });
+        });
+        console.log(allTitlesInaPage);
+        isTherePage = await page.$(".next");
+        await page.click(".next");
+    }
 }
 
 getTitles();
